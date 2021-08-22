36877dd30d7f7b8beacc884a771490288c8e95ef
Peter Stradinger <peter.stradinger@logicmonitor.com>
2021-08-22 13:39:53 -0700

    returning proper colors

--- src/Member.jsx
+++ src/Member.jsx
@@ -4,8 +4,12 @@ export const Member = ({ isDriver, initials }) => {
   return <Tile isDriver={isDriver}>{initials}</Tile>;
 };
 
+const calcColor = ({ isDriver }) => {
+  return isDriver ? "#5a0d51" : "#17435e";
+};
+
 const Tile = styled.div`
-  background-color: #{(isDriver) => ? #5a0d51: #17435e};
+  background-color: ${calcColor};
   color: #ddd;
   width: 32px;
   height: 32px;
