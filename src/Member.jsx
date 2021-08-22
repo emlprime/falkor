42240d620b09ddfffd642e45bcb2ba16a1603a3a
Peter Stradinger <peter.stradinger@logicmonitor.com>
2021-08-22 13:35:02 -0700

    renders initials

--- src/Member.jsx
+++ src/Member.jsx
@@ -1,3 +1,16 @@
-export const Member = () => {
-  return <div>Member</div>;
+import styled from "styled-components";
+
+export const Member = ({ isDriver, initials }) => {
+  return <Tile isDriver={isDriver}>{initials}</Tile>;
 };
+
+const Tile = styled.div`
+  background-color: #{(isDriver) => ? #5a0d51: #17435e};
+  color: #ddd;
+  width: 32px;
+  height: 32px;
+  border-radius: 30%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
