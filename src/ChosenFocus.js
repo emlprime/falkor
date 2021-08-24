860afdba03421fa74827516a6fec81b22236b489
Peter Stradinger <peter.stradinger@logicmonitor.com>
2021-08-23 21:27:56 -0700

    All hail Elaina's awesome math trig skillz

--- src/ChosenFocus.js
+++ src/ChosenFocus.js
@@ -5,7 +5,7 @@ function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
   var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
 
   return {
-    x: centerX + radius * Math.cos(angleInRadians),
+    x: centerX + radius * -1 * Math.cos(angleInRadians),
     y: centerY + radius * Math.sin(angleInRadians),
   };
 }
@@ -16,7 +16,7 @@ function describeArc(x, y, radius, startAngle, endAngle) {
 
   var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
 
-  var d = ["M", start.x, start.y, "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y].join(" ");
+  var d = ["M", start.x, start.y, "A", radius, radius, 0, largeArcFlag, 1, end.x, end.y].join(" ");
 
   return d;
 }
@@ -25,10 +25,14 @@ export const ChosenFocus = () => {
   const x = 200;
   const y = 200;
   const radius = 100;
-  const startAngle = 90;
-  const endAngle = 180;
+  const startAngle = 155;
+  const endAngle = 200;
   const arcConfig = describeArc(x, y, radius, startAngle, endAngle);
-  console.log("arcConfig:", arcConfig);
+  const arcConfig2 = describeArc(x, y, radius, 0, 90);
+
+  const handleClick = (value) => {
+    console.log("value:", value);
+  };
 
   return (
     <Style>
@@ -47,7 +51,21 @@ export const ChosenFocus = () => {
         >
           Week 3
         </text>
-        const <path d={arcConfig} stroke={colors.planned} strokeWidth={5} fill="transparent" />}
+        <path
+          d={arcConfig2}
+          stroke={colors.resolved}
+          strokeWidth={15}
+          fill="transparent"
+          onClick={() => handleClick("bar")}
+        />
+        <path
+          d={arcConfig}
+          stroke={colors.planned}
+          strokeWidth={15}
+          fill="transparent"
+          onClick={() => handleClick("foo")}
+        />
+        }
       </svg>
     </Style>
   );
