174ed0cb538538dec33877beb6f011e060b7cd25
Peter Stradinger <emlprime@gmail.com>
2021-09-18 14:28:13 -0700

    curried hooks moved to global

--- src/global/hooks.js
+++ src/global/hooks.js
@@ -1,4 +1,5 @@
 import {useCallback} from "react";
+import * as R from "ramda";
 import {useDispatch, useSelector} from "react-redux";
 import global from "../global";
 import {
@@ -6,8 +7,10 @@ import {
   getCurrentByModel,
   getCurrentAncestry,
 } from "./selectors";
+import {knownStatuses as ks} from "./constants";
 
 const {actions: a} = global;
+const {append, curry, head, groupBy, pick, pipe, prop} = R;
 
 export const useSetCurrentItemByModelAndStatus = model => {
   console.log(`model:`, model);
@@ -43,3 +46,54 @@ export const useSetCurrentScopeAndId = (scope, id) => {
   };
   return handleClick;
 };
+
+const deriveFirstItemOfStatus = curry((itemsByStatus, ancestry, status) =>
+  append(
+    pipe(
+      prop(status),
+      head,
+      pick(["model", "id"]),
+    )(itemsByStatus),
+    ancestry,
+  ),
+);
+
+const useHandleClickStatus = curry(
+  (ancestry, itemsByStatus, deriveSetCurrentByStatus, status) => {
+    const dispatch = useDispatch();
+    return useCallback(() => {
+      dispatch(a.setCurrentAncestry(deriveSetCurrentByStatus(status)));
+    }, [dispatch, ancestry, itemsByStatus]);
+  },
+);
+
+export const useSetCurrentAncestry = curry((getByParentKey, ancestry) => {
+  const parentKey = head(ancestry);
+  const items = useSelector(getByParentKey(parentKey));
+  const itemsByStatus = groupBy(prop("status"), items);
+
+  // curry method to get the first item of a given status for this model
+  const deriveSetCurrentByStatus = deriveFirstItemOfStatus(
+    itemsByStatus,
+    ancestry,
+  );
+
+  // curry method to get the appropriate handle click for a given status
+  const deriveHandleClickForStatus = useHandleClickStatus(
+    ancestry,
+    itemsByStatus,
+    deriveSetCurrentByStatus,
+  );
+
+  // pre calc one method for each known status
+  const handleClickResolved = deriveHandleClickForStatus(ks.resolved);
+  const handleClickActive = deriveHandleClickForStatus(ks.active);
+  const handleClickPlanned = deriveHandleClickForStatus(ks.planned);
+
+  // an object that converts the status into a dispatched click handler
+  return {
+    [ks.resolved]: handleClickResolved,
+    [ks.active]: handleClickActive,
+    [ks.planned]: handleClickPlanned,
+  };
+});
--- src/quarters/hooks.js
+++ src/quarters/hooks.js
@@ -1,54 +1,6 @@
-import * as R from "ramda";
-import {useCallback} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import global from "../global";
-import {knownStatuses as ks} from "../global/constants";
 import {getByParentKey} from "./selectors";
+import {useSetCurrentAncestry as useSetCurrentAncestryGlobal} from "../global/hooks";
 
-const {actions: a} = global;
-const {append, curry, head, groupBy, pick, pipe, prop} = R;
-
-const deriveFirstItemOfStatus = curry((itemsByStatus, ancestry, status) =>
-  append(
-    pipe(
-      prop(status),
-      head,
-      pick(["model", "id"]),
-    )(itemsByStatus),
-    ancestry,
-  ),
+export const useSetCurrentAncestry = useSetCurrentAncestryGlobal(
+  getByParentKey,
 );
-
-const useHandleClickStatus = curry(
-  (ancestry, itemsByStatus, deriveSetCurrentByStatus, status) => {
-    const dispatch = useDispatch();
-    return useCallback(() => {
-      dispatch(a.setCurrentAncestry(deriveSetCurrentByStatus(status)));
-    }, [dispatch, ancestry, itemsByStatus]);
-  },
-);
-
-export const useSetCurrentAncestry = ancestry => {
-  const parentKey = head(ancestry);
-  const items = useSelector(getByParentKey(parentKey));
-  const itemsByStatus = groupBy(prop("status"), items);
-
-  const deriveSetCurrentByStatus = deriveFirstItemOfStatus(
-    itemsByStatus,
-    ancestry,
-  );
-  const deriveHandleClickForStatus = useHandleClickStatus(
-    ancestry,
-    itemsByStatus,
-    deriveSetCurrentByStatus,
-  );
-  const handleClickResolved = deriveHandleClickForStatus(ks.resolved);
-  const handleClickActive = deriveHandleClickForStatus(ks.active);
-  const handleClickPlanned = deriveHandleClickForStatus(ks.planned);
-
-  return {
-    [ks.resolved]: handleClickResolved,
-    [ks.active]: handleClickActive,
-    [ks.planned]: handleClickPlanned,
-  };
-};
